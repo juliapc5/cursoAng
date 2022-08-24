87312c480da5b8d00df48c55b1594c8f4f051504
juliapc5 <95503102+juliapc5@users.noreply.github.com>
2022-08-24 17:35:56 -0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { LoggingService } from './logging.service';
+import { StoreModule } from '@ngrx/store';
+import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, 
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
+    StoreModule.forRoot({shoppingList: ShoppingListReducer})
+  ],
+  bootstrap: [AppComponent],
+  //providers: [LoggingService]
+
+})
+export class AppModule { }
